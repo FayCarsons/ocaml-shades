67fbf21ca022e4df07c4b290c9bb3dbdea9044f7
Fay Carsons <faycarsons23@gmail.com>
2024-01-14 15:58:56 -0500

    First commit - palettes and demo

---
+++ lib/palettes.mli
@@ -0,0 +1,115 @@
+type color = float * float * float
+
+module Met : sig
+  val manet : color list
+  val hiroshige : color list
+  val veronese : color list
+  val navajo : color list
+  val isfahan1 : color list
+  val renoir : color list
+  val vangogh1 : color list
+  val tsimshian : color list
+  val nizami : color list
+  val lakota : color list
+  val signac : color list
+  val hokusai1 : color list
+  val hokusai3 : color list
+  val juarez : color list
+  val pissaro : color list
+  val vangogh3 : color list
+  val okeeffe2 : color list
+  val homer1 : color list
+  val pillement : color list
+  val monet : color list
+  val thomas : color list
+  val homer2 : color list
+  val archambault : color list
+  val moreau : color list
+  val wissing : color list
+  val johnson : color list
+  val cassatt1 : color list
+  val newkingdom : color list
+  val kandinsky : color list
+  val austria : color list
+  val peru1 : color list
+  val vangogh2 : color list
+  val greek : color list
+  val benedictus : color list
+  val java : color list
+  val tam : color list
+  val egypt : color list
+  val redon : color list
+  val klimt : color list
+  val morgenstern : color list
+  val tara : color list
+  val troy : color list
+  val peru2 : color list
+  val okeeffe1 : color list
+  val gauguin : color list
+  val paquin : color list
+  val hokusai2 : color list
+  val isfahan2 : color list
+  val tiepolo : color list
+  val nattier : color list
+end
+
+module Pnw : sig
+  val moth : color list
+  val winter : color list
+  val spring : color list
+  val sailboat : color list
+  val mushroom : color list
+  val sunset2 : color list
+  val sunset : color list
+  val shuksan : color list
+  val lake : color list
+  val shuksan2 : color list
+  val bay : color list
+  val cascades : color list
+  val starfish : color list
+  val anemone : color list
+end
+
+module WesAnderson : sig
+  val zissou1 : color list
+  val darjeeling1 : color list
+  val moonrise3 : color list
+  val rushmore : color list
+  val bottlerocket2 : color list
+  val bottlerocket1 : color list
+  val royal2 : color list
+  val moonrise2 : color list
+  val grandbudapest2 : color list
+  val isleofdogs1 : color list
+  val chevalier1 : color list
+  val isleofdogs2 : color list
+  val moonrise1 : color list
+  val darjeeling2 : color list
+  val cavalcanti1 : color list
+  val frenchdispatch : color list
+  val rushmore1 : color list
+  val grandbudapest1 : color list
+  val fantasticfox1 : color list
+  val royal1 : color list
+end
+
+module MexBrewer : sig
+  val casita1 : color list
+  val taurus1 : color list
+  val atentado : color list
+  val naturaleza : color list
+  val concha : color list
+  val casita3 : color list
+  val vendedora : color list
+  val tierra : color list
+  val ronda : color list
+  val alacena : color list
+  val huida : color list
+  val taurus2 : color list
+  val ofrenda : color list
+  val casita2 : color list
+  val revolucion : color list
+  val aurora : color list
+  val frida : color list
+  val maiz : color list
+end
